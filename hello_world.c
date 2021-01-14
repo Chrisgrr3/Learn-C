a41db28d4e6eb421b1f13000755b04d0a1428cf1
Christopher Guerrero <chrisgrr3@gmail.com>
2021-01-13 16:43:38 -0900

    Added comments to hello_world.c

--- hello_world.c
+++ hello_world.c
@@ -1,5 +1,9 @@
+// C is a compiled language
+// To use the most basic function, printf, which is defined in the stdio.h header file, 
+// we must have this in our first line of code:
 #include <stdio.h>
 
+// Next, the first code that will run resides in the main function.
 void main() {
     printf("Hello, World!");
 }
