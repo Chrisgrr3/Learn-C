765b41a1095f4c417b900dc7828f7e5ee32948e4
Christopher Guerrero <chrisgrr3@gmail.com>
2021-01-14 20:54:44 -0900

    Update for_loops.c
    Created an example using a for loop to calculate the factorial of an array.

--- Notes/for_loops.c
+++ Notes/for_loops.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
-void main() {
+void main()
+{
     // For loops allow us ability to create a code block that runs multiple times + require an iterator variable.
     // For loops give the following functionality:
     // Initialize iterator varaible using initial value.
@@ -10,8 +11,21 @@ void main() {
     int array[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
     int sum = 0;
     int i;
-    for (i = 0; i < 10; i++) {
+    for (i = 0; i < 10; i++)
+    {
         sum += array[i];
     }
     printf("The sum of the array is %d\n", sum);
+
+    // Here is an example using a for loop to find the factorial of an array:
+    int array[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    int factorial = 1;
+    int i;
+
+    /* calculate the factorial using a for loop  here*/
+    for (i = 0; i < 10; i++)
+    {
+        factorial *= array[i];
+    }
+    printf("10! is %d.\n", factorial);
 }
