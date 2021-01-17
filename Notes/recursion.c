85a970443ca076a5ca53b5c600545122b445919a
Christopher Guerrero <chrisgrr3@gmail.com>
2021-01-16 19:19:02 -0900

    Created the factorial function using recursion

--- Notes/recursion.c
+++ Notes/recursion.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 unsigned int multiply(unsigned int x, unsigned int y);
+unsigned int factorial(unsigned int z);
 void main()
 {
     // Recursion is when a function contains within it a call to itself. I can results in very neat and elegant code, but can result in a very large amount of memory being used if the recursion gets too deep.
@@ -11,7 +12,13 @@ void main()
     // A call to itself that must make progress towards the terminating case.
 
     // View the multiply function for an example of recursion.
-    printf("3 times 5 is %d", multiply(3, 5));
+    printf("3 times 5 is %d\n", multiply(3, 5));
+
+    // View the factorial function for another example of recursion.
+    printf("0! = %i\n", factorial(0));
+    printf("1! = %i\n", factorial(1));
+    printf("3! = %i\n", factorial(3));
+    printf("5! = %i\n", factorial(5));
 }
 unsigned int multiply(unsigned int x, unsigned int y)
 {
@@ -27,4 +34,15 @@ unsigned int multiply(unsigned int x, unsigned int y)
     }
     return 0;
     // This is a catch scenario and occurs when x is zero.
+}
+unsigned int factorial(unsigned int z)
+{
+    if (z == 0)
+    {
+        return 1;
+    }
+    else if (z >= 1)
+    {
+        return z * (factorial(z - 1));
+    }
 }
